d2b8921a17d25a2a38d4d652f9f78f0c78d54c15
Greg Gan <ggreg96@gmail.com>
2018-07-29 11:34:49 +1000

    connect to aws mysql

---
+++ connect_to_dbs.js
@@ -0,0 +1,25 @@
+var express    = require("express");
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'youthservice.cnmbl5wsbdbo.us-east-2.rds.amazonaws.com',
+  port     : '3306',
+  user     : 'youth_service',
+  password : 'youth123',
+  database : 'brisbane_youth_service'
+
+});
+var app = express();
+
+connection.connect(function(err){
+
+if(!err) {
+    console.log("Database is connected ... ");    
+} else {
+    console.log("Error connecting database ... ")
+	console.log(err);    
+}
+});
+
+connection.query('SELECT * from worker', function (error, results, fields) {
+  if (error) throw error;
+});
